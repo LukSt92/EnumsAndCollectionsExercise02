462303cc301615510a5b23d684ec7a16fb543dcb
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-08-16 14:29:16 +0200

    Add: Shoe class and enum with different styles

---
+++ EnumsAndCollectionsExercise02/Shoe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsAndCollectionsExercise02
+{
+    class Shoe
+    {
+        public Shoe(Style style, string color)
+        {
+            Style = style;
+            Color = color;
+        }
+        public Style Style { get; private set; }
+        public string Color { get; private set; }
+        public string Description
+        {
+            get { return $"A {Color} {Style}"; }
+        }
+
+    }
+}
---
+++ EnumsAndCollectionsExercise02/Style.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsAndCollectionsExercise02
+{
+    enum Style {
+        Sneaker,
+        Loafer,
+        Sandal,
+        Flipflop,
+        Wingtip,
+        Clog,
+    }
+}
